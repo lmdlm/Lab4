e7481a0c06e0fb0e49ddbe3b09498001b54b1170
lmdlm <g00363430@gmit.ie>
2019-02-13 15:48:36 +0000

    last commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { BOOL_TYPE } from '@angular/compiler/src/output/output_ast';
+import { isBoolean } from 'util';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
+ 
 })
 export class AppComponent {
   title = 'data-binding-app';
+  counter:number = 0;
+  hide:boolean = true;
+  
+
+  onButtonClick()
+  {
+    console.log("button clicked");
+    this.counter++;
+  }
+
+  onDoubleButtonClick()
+  {
+    if(this.hide == true)
+    {
+      this.hide = false;
+    }
+    else{
+      this.hide = true;
+    }
+  }
 }
---
+++ src/app/list/list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html'
+})
+export class ListComponent implements OnInit {
+  presidents:string[] = ["Higgins", "McAleese", "Robinson", "Hillery"];
+  
+
+  presidents2: object[] =
+[{name: "Higgins", term:"2011-"},
+{name: "McAleese", term:"1997-2011"},
+{name: "Robinson", term:"1990-1997"},
+{name: "Hillery", term:"1976-1990"},
+{name: "O Dalaigh", term:"1974-1976"}];
+
+  constructor() {
+    this.presidents.push("Martin");
+   }
+
+
+  ngOnInit() {
+  }
+
+}
